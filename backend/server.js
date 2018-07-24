a25fd00dc93bb7505b7b861b2ee4490eff141aed
Jesse M. Holmes <wolfhoundjesse@gmail.com>
2018-07-24 14:37:00 -0400

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+import { getSecret } from './secrets';
+import Comment from './models/comment';
+
+// and create our instances
+const app = express();
+const router = express.Router();
+
+// set our port to either a predetermined port number if you have set it up, or 3001
+const API_PORT = process.env.API_PORT || 3001;
+
+console.log(getSecret('dbUri'))
+mongoose.connect(getSecret('dbUri'), {uri_decode_auth: true, useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// now we should configure the API to use bodyParser and look for JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+// now we can set the route path & initialize the API
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+router.get('/comments', (req, res) => {
+  Comment.find((err, comments) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: comments });
+  });
+});
+
+router.post('/comments', (req, res) => {
+  const comment = new Comment();
+  const { author, text } = req.body;
+  if (!author || !text) {
+    return res.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+  comment.author = author;
+  comment.text = text;
+  comment.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ succes: true });
+  });
+}); 
+// Use our router configuration when we call /api
+app.use('/api', router);
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
---
+++ client/src/CommentBox.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import 'whatwg-fetch';
+import CommentList from './CommentList';
+import CommentForm from './CommentForm';
+import './CommentBox.css';
+
+class CommentBox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      error: null,
+      author: '',
+      text: ''
+    };
+    this.pollInterval = null;
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    if (!this.pollInterval) {
+      this.pollInterval = setInterval(this.loadCommentsFromServer, 2000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.pollInterval) clearInterval(this.pollInterval);
+    this.pollInterval = null;
+  }
+
+  loadCommentsFromServer = () => {
+    fetch('/api/comments/')
+      .then(data => data.json())
+      .then(res => {
+        if (!res.success) this.setState({ error: res.error });
+        else this.setState({ data: res.data });
+      });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="comments">
+          <h2>Comments:</h2>
+          <CommentList data={this.state.data} />
+        </div>
+        <div className="form">
+          <CommentForm author={this.state.author} text={this.state.text} />
+        </div>
+        {this.state.error && <p>{this.state.error}</p>}
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
